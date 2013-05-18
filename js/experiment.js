5e1096074a7f87762a812652ce01169f77bc8807
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-05-18 18:57:47 +0200

    log click events

--- js/experiment.js
+++ js/experiment.js
@@ -2,10 +2,24 @@
     var logger = thx.loggerFor('log', 'Experiment');
     var wrapErrorHandlerAround = thx.wrapperWith(logger);
 
+    var clickEventMapper = function(event){
+	logger.log('mapping event');
+	return {
+	    type : event.type,
+	    x : event.clientX,
+	    y : event.clientY
+	}
+    }
+
     try {
 	var db = Pouch('events');
 
+	var capture = document.getElementById('capture');
+	capture.addEventListener('click', function(event){
+	    db.post(clickEventMapper(event));
+	});
     } catch(e) {
 	logger.log('not initalized correctly')
+	console.log(e);
     }
 })(thx, document, Pouch);
